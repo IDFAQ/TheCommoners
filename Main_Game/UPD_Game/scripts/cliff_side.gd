5304977cfdb34cb508de85ed6b887cd2f1a3ba28
AaronConnolly <c22410766@mytudublin.ie>
2024-02-12 14:18:10 +0000

    fix player 2

--- Main_Game/UPD_Game/scripts/cliff_side.gd
+++ Main_Game/UPD_Game/scripts/cliff_side.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+@onready var transition = $Transition
+@onready var color_rect = $Transition/ColorRect
+var dungeon = preload("res://scenes/dungeon.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
@@ -13,15 +15,17 @@ func _on_world_transition_body_entered(body):
 
 func _on_dungeon_transition_body_entered(body):
 	if body.has_method("player"):
+		transition.play("fade_out")
 		Global.transition_dungeon = true
 		change_scenes()
 
 func change_scenes():
+	await transition.animation_finished
 	if Global.transition_world == true:
 		if Global.current_scene == "cliff_side":
 			get_tree().change_scene_to_file("res://scenes/world.tscn")
 			Global.finish_changescene()
 	elif Global.transition_dungeon:
 		if Global.current_scene == "cliff_side":
-			get_tree().change_scene_to_file("res://scenes/dungeon.tscn")
+			get_tree().change_scene_to_packed(dungeon)
 			Global.finish_changescene()
--- Main_Game/UPD_Game/scripts/dungeon.gd
+++ Main_Game/UPD_Game/scripts/dungeon.gd
@@ -1,12 +1,8 @@
 extends Node2D
 
+@onready var transition = $Transition
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	transition.play("fade_in")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
